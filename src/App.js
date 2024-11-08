e41e201492a6a79cc041ba478bcdb955a037b1ed
daksh100sharma <daksh100sharma@gmail.com>
2024-11-08 21:49:53 +0530

    + Add responsivity for small, medium, large and < large viewports. | + Add news category now works from NavBar

--- src/App.js
+++ src/App.js
@@ -1,18 +1,15 @@
-import React, { useState, useEffect, useRef } from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 import Navbar from './components/navBar';
 import Card from './components/card';
 import axios from 'axios';
+import { Routes, Route, useParams, Navigate } from 'react-router-dom';
 
-/************************************/
-/************************************/
-const NEWS_API_KEY = 'YOUR NEWS API KEY'; // GET YOUR API KEY FROM https://newsapi.org/register
-/************************************/
-/************************************/
+const NEWS_API_KEY = '2b69528fbf99441788c31428f135c2dc';
 
-const fetchNews = async () => {
+const fetchNews = async (category) => {
   try {
-    const response = await axios.get(`https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=${NEWS_API_KEY}`);
+    const response = await axios.get(`https://newsapi.org/v2/top-headlines?country=us&category=${category}&apiKey=${NEWS_API_KEY}`);
     const articles = response.data.articles.map(article => ({
       title: article.title,
       description: article.description,
@@ -26,69 +23,44 @@ const fetchNews = async () => {
   }
 };
 
-function App() {
-  const [mousePosition, setMousePosition] = useState({ x: 0, y: 0 });
-  const [isHovered, setIsHovered] = useState(false);
-  const [tooltipText, setTooltipText] = useState('');
+function NewsCategory() {
+  const { category } = useParams('general');
   const [articles, setArticles] = useState([]);
-  const titleRef = useRef(null);
-
-  const handleMouseMove = (e) => {
-    const rect = e.currentTarget.getBoundingClientRect();
-    const x = e.clientX - rect.left;
-    const y = e.clientY - rect.top;
-    setMousePosition({ x, y });
-
-    if (titleRef.current) {
-      setTooltipText(titleRef.current.textContent);
-    }
-  };
 
   useEffect(() => {
     const getArticles = async () => {
-      const newsArticles = await fetchNews();
+      const newsArticles = await fetchNews(category);
       setArticles(newsArticles);
     };
-
     getArticles();
-  }, []);
+  }, [category]);
 
+  return (
+    <div className="h-screen flex justify-center items-start pt-10">
+      <div className="custom-height min-w-[96%] max-w-screen-xl border-4 border-spacing-5 border-dashed border-slate-300 rounded-lg grid gap-5 p-5 
+                      grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5">
+        {articles.map((article, index) => (
+          <Card
+            key={index}
+            title={article.title}
+            description={article.description}
+            urlToImg={article.urlToImg || 'https://www.niijiiradio.com/wp-content/uploads/2016/10/News-Image-3.jpg'}
+            url={article.url}
+          />
+        ))}
+      </div>
+    </div>
+  );
+}
+
+function App() {
   return (
     <div>
       <Navbar />
-      <div className="h-screen flex justify-center items-start pt-10">
-        <div
-          className="custom-height min-w-[96%] max-w-screen-xl border-4 border-spacing-5 border-dashed border-slate-300 rounded-lg grid grid-cols-[repeat(4,1fr)] gap-5 p-5"
-          onMouseMove={handleMouseMove}
-        >
-
-          {articles.map((article, index) => (
-            article.title !== '[Removed]' && (
-              <Card
-                key={index}
-                title={article.title}
-                description={article.description}
-                urlToImg={article.urlToImg || 'https://www.niijiiradio.com/wp-content/uploads/2016/10/News-Image-3.jpg'}
-                url={article.url}
-              />
-            )
-          ))}
-
-          {isHovered && tooltipText && (
-            <div
-              className="absolute text-sm text-gray-700 p-1 bg-white border border-gray-300 rounded-md shadow-lg"
-              style={{
-                top: mousePosition.y + 50,
-                left: mousePosition.x + 20,
-                pointerEvents: 'none',
-                zIndex: 1000,
-              }}
-            >
-              {tooltipText}
-            </div>
-          )}
-        </div>
-      </div>
+      <Routes>
+        <Route path="/" element={<Navigate to="/general" replace />} />
+        <Route path="/:category" element={<NewsCategory />} />
+      </Routes>
     </div>
   );
 }
